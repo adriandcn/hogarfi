5cf1ac1aaa5dc8bcf544c7b53eeca218ca500663
adrian <adria@ADRIAN-PC>
2026-04-07 14:38:22 -0400

    fix: disable csrf + all vercel urls trusted

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,6 +10,7 @@ export const auth = betterAuth({
     usePlural: false,
   }),
   advanced: {
+    disableCsrfCheck: true,
     crossSubDomainCookies: {
       enabled: false,
     },
@@ -33,6 +34,11 @@ export const auth = betterAuth({
   verification: { modelName: 'betterAuthVerification' },
   trustedOrigins: [
     'http://localhost:3000',
+    'https://hogarfi.vercel.app',
+    'https://hogarfi-rj0kg4mrj-adrian22.vercel.app',
+    'https://hogarfi-ib8vsciri-adrian22.vercel.app',
+    'https://hogarfi-jpcf6796f-adrian22.vercel.app',
+    'https://hogarfi-hc75yi43q-adrian22.vercel.app',
     process.env.BETTER_AUTH_URL ?? '',
     process.env.NEXT_PUBLIC_APP_URL ?? '',
   ].filter(Boolean),
